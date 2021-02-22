e2d8826b7aa790f09d874bf362fb9e7461563bfd
Katy La Tour <klatour324@gmail.com>
2021-02-22 12:56:04 -0600

    Add routes for editing and updating the mechanic show page with adding a ride to workload

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   get "/mechanics", to: "mechanics#index"
   get "/mechanics/:id", to: "mechanics#show"
+  get "mechanics/:id/edit", to: "mechanics#edit"
+  patch "mechanics/:id", to: "mechanics#update"
 end
