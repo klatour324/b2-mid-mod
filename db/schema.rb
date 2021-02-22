7e4b5fafbbe353efed3a29e5a772536813dbe45e
Katy La Tour <klatour324@gmail.com>
2021-02-22 11:30:18 -0600

    Add seeds record data to seeds file

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_162646) do
+ActiveRecord::Schema.define(version: 2021_02_22_164733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "mechanics", force: :cascade do |t|
-    t.string "name"
     t.integer "years_experience"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
   end
 
   create_table "ride_mechanics", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+RideMechanic.destroy_all
+Ride.destroy_all
+Mechanic.destroy_all
+
+mechanic1 = Mechanic.create!(first_name: "Joey", last_name: "Smith", years_experience: 20)
+mechanic2 = Mechanic.create!(first_name: "Mark", last_name: "Doe", years_experience: 10)
+mechanic3 = Mechanic.create!(first_name: "Lexi", last_name: "Bea", years_experience: 15)
+
+ride1 = mechanic1.rides.create!(name: "Whirl & Hurl", thrill_rating: 9, open: false)
+ride2 = mechanic1.rides.create!(name: "Toppling Goliath", thrill_rating: 6, open: true)
+ride3 = mechanic3.rides.create!(name: "The Viper", thrill_rating: 7, open: true)
