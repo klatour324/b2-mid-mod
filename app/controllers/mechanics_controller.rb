89fca8ea8758561d7305295b36d6d73e94d209f5
Katy La Tour <klatour324@gmail.com>
2021-02-22 12:56:24 -0600

    Add edit and update actions to controller

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -6,4 +6,19 @@ class MechanicsController < ApplicationController
   def show
     @mechanic = Mechanic.find(params[:id])
   end
+
+  def edit
+    @mechanic = Mechanic.find(params[:id])
+  end
+
+  def update
+    mechanic = Mechanic.find(params[:id])
+    mechanic.update!(mechanic_params)
+    redirect_to "/mechanics/#{mechanic.id}"
+  end
+
+  private
+  def mechanic_params
+    params.permit(:first_name, :last_name, :years_of_experience, :ride_name)
+  end
 end
