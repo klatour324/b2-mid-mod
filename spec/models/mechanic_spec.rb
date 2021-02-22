f2d60754049300a306f2e16aed8217f8026adfd0
Katy La Tour <klatour324@gmail.com>
2021-02-22 11:31:59 -0600

    Create model test for average of years of experience for all mechanics

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe Mechanic, type: :model do
     it { should have_many :ride_mechanics }
     it { should have_many(:rides).through(:ride_mechanics) }
   end
+
+  describe "class methods" do
+    describe "::average_years_of_experience" do
+      it "can find average years of experience for all mechanics" do
+        mechanic1 = Mechanic.create!(first_name: "Joey", last_name: "Smith", years_experience: 20)
+        mechanic2 = Mechanic.create!(first_name: "Mark", last_name: "Doe", years_experience: 10)
+        mechanic3 = Mechanic.create!(first_name: "Lexi", last_name: "Bea", years_experience: 15)
+
+        expect(Mechanic.average_years_of_experience).to eq(15)
+      end
+    end
+  end
 end
